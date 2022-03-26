ac8e7359af1260a2ecf39cb76116c0bf00b813d6
Priyanshu Vaishnavi <vaishnavipriyanshu198@gmail.com>
2022-03-26 11:25:58 +0530

    footer_updated

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
       <!-- footer section -->
       <nav class="navbar navbar-expand-lg navbar-light bg-light border-top">
           <div class="container m-auto">
-            <a href="#" class="m-auto" style="text-decoration: none;">Made With &#10084; by Krupesh Vithlani</a>
+            <a href="#" class="m-auto" style="text-decoration: none;">Designed With <i class="fa fa-heart pulse"></i> by Krupesh Vithlani</a>
           </div>
         </nav>
         <!-- footer section over -->
